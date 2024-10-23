8a35644fe0b058ecdf3227b30221410f0e535f12
Tontoon7 <axel.chevenement@gmail.com>
2024-10-23 18:22:32 +0200

    modified header and layout

--- components/Header.js
+++ components/Header.js
@@ -1,13 +1,23 @@
 import Link from 'next/link';
+import Image from 'next/image';
+import TechACMLogo from './techacm-logo.svg';
 
 export default function Header({ name }) {
   return (
     <header className="pt-20 pb-12">
-      <div className="block w-12 h-12 mx-auto mb-4 rounded-full bg-gradient-conic from-gradient-3 to-gradient-4" />
-      <p className="text-2xl text-center dark:text-white">
+      <div className="w-24 h-24 mx-auto mb-4">
         <Link href="/">
-          {name}
+          <Image
+            src={TechACMLogo}
+            alt="TechACM Logo"
+            width={96}
+            height={96}
+            className="rounded-full"
+          />
         </Link>
+      </div>
+      <p className="text-2xl text-center dark:text-white">
+        {name}
       </p>
     </header>
   );
--- pages/index.js
+++ pages/index.js
@@ -13,15 +13,15 @@ export default function Index({ posts, globalData }) {
     <Layout>
       <SEO title={globalData.name} description={globalData.blogTitle} />
       <Header name={globalData.name} />
-      <main className="w-full">
+      <main className="w-[1200px] mx-auto">
         <h1 className="mb-12 text-3xl text-center lg:text-5xl">
           {globalData.blogTitle}
         </h1>
-        <ul className="w-full">
+        <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 w-full col-width-1200 mx-auto">
           {posts.map((post) => (
             <li
               key={post.filePath}
-              className="transition bg-white border border-b-0 border-gray-800 md:first:rounded-t-lg md:last:rounded-b-lg backdrop-blur-lg dark:bg-black dark:bg-opacity-30 bg-opacity-10 hover:bg-opacity-20 dark:hover:bg-opacity-50 dark:border-white border-opacity-10 dark:border-opacity-10 last:border-b hover:border-b hovered-sibling:border-t-0"
+              className="transition h-[400px] rounded-lg overflow-hidden bg-white border border-b-0 border-gray-800 md:first:rounded-t-lg md:last:rounded-b-lg backdrop-blur-lg dark:bg-black dark:bg-opacity-30 bg-opacity-10 hover:bg-opacity-20 dark:hover:bg-opacity-50 dark:border-white border-opacity-10 dark:border-opacity-10 last:border-b hover:border-b hovered-sibling:border-t-0"
             >
               <Link
                 as={`/posts/${post.filePath.replace(/\.mdx?$/, '')}`}
